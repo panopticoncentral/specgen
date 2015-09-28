03fbb543324be589f80295155704c550d23bce1b
Paul Vick <paul@panopticoncentral.net>
2015-09-28 16:57:25 -0700

    Initial project.

---
+++ src/specgen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace specgen
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("specgen: Invalid command-line arguments.");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
