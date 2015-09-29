8d8e42e3ba1606b9cff69f4ae205d9117e7c98f9
Paul Vick <paul@panopticoncentral.net>
2015-09-28 17:27:32 -0700

    Package wrapper

--- src/specgen/Program.cs
+++ src/specgen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace specgen
 {
@@ -6,11 +7,21 @@ namespace specgen
     {
         static int Main(string[] args)
         {
-            if (args.Length > 0)
+            if (args.Length != 2)
             {
-                Console.WriteLine("specgen: Invalid command-line arguments.");
+                Console.WriteLine("Usage: specgen <input path> <output path>");
                 return 1;
             }
+
+            var pkg = (XNamespace)"http://schemas.microsoft.com/office/2006/xmlPackage";
+
+            var doc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XProcessingInstruction("mso-application", "progid=\"Word.Document\""),
+                new XElement(pkg + "package",
+                    new XAttribute(XNamespace.Xmlns + "pkg", pkg.NamespaceName)));
+
+            doc.Save(args[1]);
 
             return 0;
         }
