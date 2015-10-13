2ad090c12bca653e298840f6a7d3ede10febd5bf
Paul Vick <paul@panopticoncentral.net>
2015-10-12 17:08:24 -0700

    Specification checking.

--- src/specgen/Program.cs
+++ src/specgen/Program.cs
@@ -1268,6 +1268,132 @@ namespace specgen
                     new XElement(W + "allowPNG")));
         }
 
+        private static void CheckSpecification(XDocument spec)
+        {
+            var tokenSymbols = new Dictionary<string, bool>();
+            var tokens =
+                (from t in spec.Descendants("token")
+                where t.Attribute("ignore") == null || !bool.Parse(t.Attribute("ignore").Value)
+                select t).ToList();
+            var tokenNames = from t in tokens select t.Attribute("name").Value;
+
+            foreach (var name in tokenNames)
+            {
+                if (tokenSymbols.ContainsKey(name))
+                {
+                    Console.WriteLine($"Error: Duplicate token name'{name}'.");
+                }
+                else
+                {
+                    tokenSymbols[name] = false;
+                }
+            }
+
+            var tokenReferences =
+                from t in tokens
+                from nt in t.Descendants("nt")
+                select nt.Value;
+            var missingTokenReferences = new HashSet<string>();
+
+            foreach (var tokenReference in tokenReferences)
+            {
+                if (!tokenSymbols.ContainsKey(tokenReference))
+                {
+                    missingTokenReferences.Add(tokenReference);
+                }
+                else
+                {
+                    tokenSymbols[tokenReference] = true;
+                }
+            }
+
+            foreach (var missingToken in missingTokenReferences.OrderBy(v => v))
+            {
+                Console.WriteLine($"Error: Token reference to missing token '{missingToken}'.");
+            }
+
+            var syntaxSymbols = new Dictionary<string, bool>();
+            var syntaxes =
+                (from t in spec.Descendants("syntax")
+                where t.Attribute("ignore") == null || !bool.Parse(t.Attribute("ignore").Value)
+                select t).ToList();
+            var syntaxNames = from t in syntaxes select t.Attribute("name").Value;
+
+            foreach (var name in syntaxNames)
+            {
+                if (syntaxSymbols.ContainsKey(name))
+                {
+                    if (name != "start")
+                    {
+                        Console.WriteLine($"Error: Duplicate syntax name '{name}'.");
+                    }
+                    else
+                    {
+                        syntaxSymbols[name] = false;
+                    }
+                }
+
+                if (tokenSymbols.ContainsKey(name) && name != "start")
+                {
+                    Console.WriteLine($"Error: Duplicate token/syntax name '{name}'.");
+                }
+            }
+
+            var syntaxReferences =
+                from t in syntaxes
+                from nt in t.Descendants("nt")
+                select nt.Value;
+            var missingSyntaxReferences = new HashSet<string>();
+
+            foreach (var syntaxReference in syntaxReferences)
+            {
+                if (!syntaxSymbols.ContainsKey(syntaxReference))
+                {
+                    if (!tokenSymbols.ContainsKey(syntaxReference))
+                    {
+                        missingSyntaxReferences.Add(syntaxReference);
+                    }
+                    else
+                    {
+                        tokenSymbols[syntaxReference] = true;
+                    }
+                }
+                else
+                {
+                    syntaxSymbols[syntaxReference] = true;
+                }
+            }
+
+            foreach (var missingSyntax in missingSyntaxReferences.OrderBy(v => v))
+            {
+                Console.WriteLine($"Error: Syntax reference to missing syntax '{missingSyntax}'.");
+            }
+
+            if (missingSyntaxReferences.Count > 0)
+            {
+                Console.WriteLine($"Error: Missing {missingSyntaxReferences.Count} syntax references.");
+            }
+
+            foreach (var tokenSymbol in tokenSymbols.Where(v => v.Key != "start" && !v.Value))
+            {
+                Console.WriteLine($"Error: Token '{tokenSymbol.Key}' is never referenced.");
+            }
+
+            foreach (var syntaxSymbol in syntaxSymbols.Where(v => v.Key != "start" && !v.Value))
+            {
+                Console.WriteLine($"Error: Syntax '{syntaxSymbol.Key}' is never referenced.");
+            }
+
+            var nameReferences = from r in spec.Descendants("ref") select r.Value;
+
+            foreach (
+                var nameReference in
+                    nameReferences.Where(nr => !tokenSymbols.ContainsKey(nr) && !syntaxSymbols.ContainsKey(nr)))
+            {
+                Console.WriteLine($"Error: Missing name reference '{nameReference}.");
+            }
+        }
+
         static int Main(string[] args)
         {
             if (args.Length != 2)
@@ -1275,6 +1401,12 @@ namespace specgen
                 Console.WriteLine("Usage: specgen <input path> <output path>");
                 return 1;
             }
+
+            var spec = XDocument.Load(args[0]);
+
+            Console.WriteLine("Checking specification...");
+            CheckSpecification(spec);
+            Console.WriteLine("Checked specification...");
 
             var doc = new XDocument(
                 new XDeclaration("1.0", "UTF-8", "yes"),
