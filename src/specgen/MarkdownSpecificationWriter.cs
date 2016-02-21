fa7e9a5883b67a3a73a6a54f90922ee6d83ccd70
Paul Vick <paulv@microsoft.com>
2016-02-20 19:16:33 -0700

    Markdown Table of Contents

--- src/specgen/MarkdownSpecificationWriter.cs
+++ src/specgen/MarkdownSpecificationWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace specgen
@@ -15,7 +16,30 @@ namespace specgen
             writer.WriteLine();
             writer.WriteLine(head?.Element("draft")?.Value ?? head?.Element("date")?.Value);
             writer.WriteLine();
+            writer.WriteLine(head?.Element("author")?.Value);
+            writer.WriteLine();
             writer.WriteLine("<br/>");
+            writer.WriteLine();
+        }
+
+        private static void TableOfContentsLevel(XElement level, TextWriter writer, int indent, string parentLevel)
+        {
+            var sections = level.Elements("section");
+
+            var index = 1;
+            foreach (var section in sections)
+            {
+                writer.WriteLine($"{new string(' ', indent * 2)}* [{parentLevel}{index} {section.Attribute("title").Value}](#{parentLevel}{index})");
+                TableOfContentsLevel(section, writer, indent + 1, $"{parentLevel}{index}.");
+                index++;
+            }
+        }
+
+        private static void TableOfContents(XDocument spec, TextWriter writer)
+        {
+            writer.WriteLine("## Table of Contents");
+            writer.WriteLine();
+            TableOfContentsLevel(spec.Elements("specification").Elements("body").Single(), writer, 0, "");
         }
 
         public static void WriteSpecification(XDocument spec, string path)
@@ -25,6 +49,7 @@ namespace specgen
                 using (var writer = new StreamWriter(stream))
                 {
                     TitleSection(spec, writer);
+                    TableOfContents(spec, writer);
                 }
             }
         }
